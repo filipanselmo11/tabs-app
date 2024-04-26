add2d5335331b09772e0298a4a58a399a29deb22
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2024-04-26 13:36:00 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tabs-app',
+  webDir: 'www'
+};
+
+export default config;
